6f9fda3c062c5928fb3e393186ca2e5994d79652
joaco0o0 <131784799+joaco0o0@users.noreply.github.com>
2023-09-13 17:53:08 -0300

    Add files via upload

---
+++ Airport/src/test/java/uy/edu/um/airport/AirportApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.edu.um.airport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
