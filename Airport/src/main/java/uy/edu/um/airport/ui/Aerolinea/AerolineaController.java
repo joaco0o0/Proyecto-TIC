e165eb03afe9781aeb87d0c6bf50f992ab1ff702
Lucas Onetto <lonetto@correo.um.edu.uy>
2023-09-24 16:18:20 -0300

    Arreglando las pantallas

--- Airport/src/main/java/uy/edu/um/airport/ui/Aerolinea/AerolineaController.java
+++ Airport/src/main/java/uy/edu/um/airport/ui/Aerolinea/AerolineaController.java
@@ -2,18 +2,18 @@ package uy.edu.um.airport.ui.Aerolinea;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import uy.edu.um.airport.entities.Aerolinea.Aerolinea;
-import uy.edu.um.airport.entities.Aerolinea.AerolineaService;
+//import uy.edu.um.airport.entities.Aerolinea.AerolineaService;
 
 import java.util.List;
 
 @RequestMapping(path = "api/v1/aerolineas")
 public class AerolineaController {
-    private final AerolineaService aerolineaService;
+    //private final AerolineaService aerolineaService;
 
-    public AerolineaController(AerolineaService aerolineaService) {
-        this.aerolineaService = aerolineaService;
-    }
-    public List<Aerolinea> getAerolineas(){
-        return aerolineaService.getAerolineas();
-    }
+    //public AerolineaController(AerolineaService aerolineaService) {
+        //this.aerolineaService = aerolineaService;
+    //}
+    //public List<Aerolinea> getAerolineas(){
+        //return aerolineaService.getAerolineas();
+    //}
 }
