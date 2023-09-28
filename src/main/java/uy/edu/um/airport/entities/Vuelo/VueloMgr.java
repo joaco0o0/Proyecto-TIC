f4409fb8c213ad4c67f85f868d5d159ebaba9efc
joaco0o0 <jheredero@correo.um.edu.uy>
2023-09-28 12:51:24 -0300

    v0.1

--- src/main/java/uy/edu/um/airport/entities/Vuelo/VueloMgr.java
+++ src/main/java/uy/edu/um/airport/entities/Vuelo/VueloMgr.java
@@ -1,8 +1,10 @@
 package uy.edu.um.airport.entities.Vuelo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import uy.edu.um.airport.persistence.VueloRepository;
 
+@Service
 public class VueloMgr {
 
     @Autowired
