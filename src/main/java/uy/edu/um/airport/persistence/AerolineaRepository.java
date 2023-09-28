3147adf716b98ec5583b866da6bcc61ac1d05989
Lucas Onetto <lonetto@correo.um.edu.uy>
2023-09-28 12:04:00 -0300

    se crea repo de la aerolinea

--- src/main/java/uy/edu/um/airport/persistence/AerolineaRepository.java
+++ src/main/java/uy/edu/um/airport/persistence/AerolineaRepository.java
@@ -1,4 +1,10 @@
 package uy.edu.um.airport.persistence;
 
-public interface AerolineaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import uy.edu.um.airport.entities.Aerolinea.Aerolinea;
+
+import java.util.Optional;
+
+public interface AerolineaRepository extends JpaRepository<Aerolinea, Long> {
+    public Optional<Aerolinea> findByNombre(String nombre);
 }
